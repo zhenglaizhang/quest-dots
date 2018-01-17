685096644ba94a3e21fc1328bdb2cf730206a132
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2018-01-17 15:16:54 +0800

    .

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,7 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update 
+
+
 sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu xenial main'
